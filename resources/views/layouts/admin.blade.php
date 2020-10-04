cc0a6fb3a3cae81ef261f660455705a2b127d108
yusufkarbackk <yusufkarback64@gmail.com>
2020-10-04 23:00:05 +0700

    menyiapkan halaman dashboard

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  @include('includes.style')
+
+</head>
+
+<body id="page-top">
+    @yield('content')
+
+    @include('includes.scripts')
+</body>
+
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+        ->namespace('Admin')
+        ->group(function() {
+            Route::get('/', [DashboardController::class, 'index'])
+            ->name('dashboard');
+        });
